1bbc4ed7ecbd2d03d9d5de665c18d8cdbc069457
clock <clock@careerintlinc.com>
2018-12-26 10:56:16 +0800

    radish agent demo

--- radish-agent/src/main/java/org/sam/shen/agent/RadishAgentApplication.java
+++ radish-agent/src/main/java/org/sam/shen/agent/RadishAgentApplication.java
@@ -2,9 +2,7 @@ package org.sam.shen.agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"org.sam.shen.core"})
 @SpringBootApplication
 public class RadishAgentApplication {
 
