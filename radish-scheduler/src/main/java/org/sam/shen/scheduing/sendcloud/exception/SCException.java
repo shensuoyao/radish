519d5dcb1e5526ddad6b154c48756c39afc337ae
clock <clock@careerintlinc.com>
2019-05-17 15:59:06 +0800

    remove sendcloud from radish-scheduler

--- radish-scheduler/src/main/java/org/sam/shen/scheduing/sendcloud/exception/SCException.java
+++
@@ -1,6 +0,0 @@
-package org.sam.shen.scheduing.sendcloud.exception;
-
-public interface SCException {
-
-	public String getMessage();
-}
--- radish-scheduler/src/main/java/org/sam/shen/scheduing/sendcloud/model/Content.java
+++
@@ -1,9 +0,0 @@
-package org.sam.shen.scheduing.sendcloud.model;
-
-import org.sam.shen.scheduing.sendcloud.exception.ContentException;
-
-public interface Content {
-	public boolean useTemplate();
-
-	public boolean validate() throws ContentException;
-}
